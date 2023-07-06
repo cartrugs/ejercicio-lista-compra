06bd800f9364e43274118628b7ef961e46456248
cartrugs <cartrugs@gmail.com>
2023-07-06 20:04:33 +0200

    Comentarios en las distintas secciones del código

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Variables
 
+// querySelector
 const leche = document.querySelector("#leche");
 const mantequilla = document.querySelector("#mantequilla");
 const cacao = document.querySelector("#cacao");
@@ -8,106 +9,89 @@ const papelHigienico = document.querySelector("#papelHigienico");
 const pastaDientes = document.querySelector("#pastaDientes");
 const azucar = document.querySelector("#azucar");
 const boton = document.querySelector(".btn");
-
 const listaVacia = document.querySelector("#listaVacia");
-
+// fragment
 const fragmento = document.createDocumentFragment();
-
+// localStorage
 let noPerderLocal = JSON.parse(localStorage.getItem("compradosArray")) || [];
-//Eventos
 
+// Eventos
 
+// Presionar botones
 document.addEventListener("click", ({ target }) => {
-  
-  if(target.classList.contains("btn")){
-   const valoresObjeto = target.id;
-   subirArrayLocal(valoresObjeto)
-   llenarVacia();
-  
+  if (target.classList.contains("btn")) {
+    const valoresObjeto = target.id;
+    subirArrayLocal(valoresObjeto);
+    llenarVacia();
   }
   if (target.classList.contains("btnQuitar")) {
-    console.log(target)
     const valoresObjeto = target.id;
-   restarArrayLocal(valoresObjeto);
-   llenarVacia();
+    restarArrayLocal(valoresObjeto);
+    llenarVacia();
   }
   if (target.classList.contains("btnQT")) {
     borrarTodo();
   }
 });
 
-
-const borrarTodo = () => {
-  
-  localStorage.clear();
-  noPerderLocal = [];
-  llenarVacia();
-}
-
-
 // Funciones
 
+// Almacenar localStorage
 const subirArrayLocal = (id) => {
-
-  const coincidencia = noPerderLocal.find((elemento) => elemento.producto == id)
-
+  const coincidencia = noPerderLocal.find((elemento) => elemento.producto == id);
   if (!coincidencia) {
     noPerderLocal.push({
       producto: id,
       cantidad: 1
-    })
-
+    });
     localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
-  }
-  else {
+  } else {
     coincidencia.cantidad++;
     localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
-    console.log(coincidencia)
+    console.log(coincidencia);
   }
+};
 
-}
-
+// Restar coincidencias
 const restarArrayLocal = (id) => {
-const coincidencia = noPerderLocal.find((elemento) => elemento.producto == id )
+  const coincidencia = noPerderLocal.find((elemento) => elemento.producto == id);
+  if (coincidencia.cantidad > 1) {
+    coincidencia.cantidad--;
+    localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
+  } else {
+    const posicionElemento = noPerderLocal.findIndex((elemento) => elemento.producto == id);
+    if (posicionElemento !== -1) {
+      noPerderLocal.splice(posicionElemento, 1);
+      localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
+    }
+  }
+};
 
-if (coincidencia.cantidad > 1 ) {
-  coincidencia.cantidad--;
-  localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
-}
+// Borrar cesta de la compra 
+const borrarTodo = () => {
+  localStorage.clear();
+  noPerderLocal = [];
+  llenarVacia();
+};
 
-else { 
-  const posicionElemento = noPerderLocal.findIndex((elemento) => elemento.producto == id)
-if(posicionElemento!==-1){
-noPerderLocal.splice(posicionElemento,1)
-localStorage.setItem("compradosArray", JSON.stringify(noPerderLocal));
-}
-}}
-
+// Pintar lista nueva
 const llenarVacia = () => {
-  listaVacia.innerHTML = ""
-
-  //const pintado = JSON.parse(localStorage.getItem("compradosArray")) || [];
+  listaVacia.innerHTML = "";
   noPerderLocal.forEach((item) => {
-
     const nuevoArticulo = document.createElement("P");
     nuevoArticulo.textContent = `${item.producto} ${item.cantidad}`;
-
-    const botonQuitar = document.createElement("BUTTON")
+    const botonQuitar = document.createElement("BUTTON");
     botonQuitar.classList.add("btnQuitar");
-    botonQuitar.textContent="quitar de la cesta";
-    botonQuitar.setAttribute("id", item.producto)
-
-
+    botonQuitar.textContent = "quitar de la cesta";
+    botonQuitar.setAttribute("id", item.producto);
     nuevoArticulo.append(botonQuitar);
     fragmento.append(nuevoArticulo);
-    
-
-  } ) 
+  });
   const botonQuitarTodo = document.createElement("BUTTON");
   botonQuitarTodo.classList.add("btnQT");
-  botonQuitarTodo.textContent="ELIMINAR TODA LA CESTA"
-
+  botonQuitarTodo.textContent = "ELIMINAR TODA LA CESTA";
   listaVacia.append(fragmento);
   listaVacia.append(botonQuitarTodo);
 };
-llenarVacia()
+
+llenarVacia();
